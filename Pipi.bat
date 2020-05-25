c9ecf460d0aab232a3b94a62b20572ebf948bf02
guy guy <you@example.com>
2020-05-25 23:43:37 +0300

    init

---
+++ Pipi.bat
@@ -0,0 +1,8 @@
+cd C:\Users\guy12\AppData\Local\Programs\Python\Python37\Scripts
+pip install pycryptodome
+pip install firebase_admin
+#pip install json
+pip install psutil
+pip intsall pathlib
+pip install netifaces
+pip install ipaddress
